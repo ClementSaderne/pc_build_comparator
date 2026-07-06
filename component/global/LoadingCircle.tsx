1dd64a4527b75abbb4955aa09649f85391d4b675
Clement Saderne <clement.saderne@gmail.com>
2026-07-06 17:50:41 +0200

    [Feature] Ajout d'un composant cercle de chargement

---
+++ component/global/LoadingCircle.tsx
@@ -0,0 +1,8 @@
+import styles from "./LoadingCircle.module.css"
+
+export default function LoadingCircle() {
+    return (
+        <span className={styles.loadingCircle}>
+        </span>
+    )
+}
